7229d1aced62f073d73b87f94ad92ce26f33db8d
Olivier Abdesselam <yazgoo@gmail.com>
2014-05-22 02:00:18 +0200

    initial commit

--- impress_arrows.js
+++ impress_arrows.js
@@ -1,93 +1,83 @@
-function do_skip(item, a)
+function impress_arrows()
 {
-    classes = []
-    do {
-        item = a(item);
-        classes = item.attr('class').split(' ');
+    function do_skip(item, a)
+    {
+        classes = []
+            do {
+                item = a(item);
+                classes = item.attr('class').split(' ');
+            }
+        while(classes.indexOf('skip') != -1)
+            return item;
     }
-    while(classes.indexOf('skip') != -1)
-    return item;
-}
-function get_radius(a)
-{
-    radius = (1.0 * (a.clientWidth < a.clientHeight ?
-        a.clientHeight : a.clientWidth)) / 2
-    console.log(a.clientWidth, a.clientHeight, radius);
-    return radius;
-}
-function get_coordinates(i)
-{
-    next = do_skip($(i), function(a) { return a.next() });
-    prev = do_skip($(i), function(a) { return a.prev() });
-    console.log(next);
-    console.log(prev);
-    xb = parseInt(next.attr('data-x'));
-    xa = parseInt(prev.attr('data-x'));
-    yb = parseInt(next.attr('data-y'));
-    ya = parseInt(prev.attr('data-y'));
-    dx = xb - xa;
-    dy = yb - ya;
-    l = Math.abs((dx == 0 ? dy : dx) * 0.4);
-    x = xa + dx / 2;
-    y = ya + dy / 2;
-    /* lets compute the middle */
-    /*
-       ra = get_radius(prev[0]);
-    rb = get_radius(next[0]);
-    D = Math.sqrt(dy * dy + dx * dx);
-    d = (D - ra - rb) / 2;
-    x = ((ra + d) / D) * dx + xa; 
-    y = ((ra + d) / D) * dy + ya; 
-    l = Math.abs(2 * d);
-    */
-    direction = Math.abs(dy) > 0 ? (dy > 0 ? 'down' : 'up') : (dx > 0 ? 'right' : 'left')
-    return [l, x, y, direction];
-}
-$(".arrow").each(function(i) {
-        p = get_coordinates(this);
-        classes = this.className.split(' ')
-        direction = way = ''
-        if(classes.indexOf('right') != -1) { way = 'right' }
-        else if(classes.indexOf('down') != -1) { way = 'down' }
-        else if(classes.indexOf('up') != -1) { way = 'up' }
-        else way = p[3];
-        direction = way == "up" || way == "down" ? 'vertical' : 'horizontal'
-        what = direction == 'horizontal' ? 'width' : 'height'
-        //p = $(this).attr('p').split(',')
-        if($(this).attr('data-x') == undefined) $(this).attr('data-x', p[1])
-        if($(this).attr('data-y') == undefined) $(this).attr('data-y', p[2])
-        css = {}; css[what] = p[0]
-        $(this).addClass('step')
-        $(this).addClass('skip')
-        $(this).css(css)
-        html = ['<div class="arrow-body-common arrow-body-' + direction + '"></div>', '<div class="arrow-end arrow-' + way + '"></div>']
-        if(way == 'left' || way == 'up') html.reverse()
-        $(this).html(html.join(""))
+    function get_coordinates(i)
+    {
+        next = do_skip($(i), function(a) { return a.next() });
+        prev = do_skip($(i), function(a) { return a.prev() });
+        xb = parseInt(next.attr('data-x'));
+        xa = parseInt(prev.attr('data-x'));
+        yb = parseInt(next.attr('data-y'));
+        ya = parseInt(prev.attr('data-y'));
+        dx = xb - xa;
+        dy = yb - ya;
+        l = Math.sqrt(dy * dy + dx * dx) * 0.4
+        x = xa + 0.51 * dx;
+        y = ya + 0.51 * dy;
+        angle = Math.atan2(yb - ya, xb - xa) * (180 / Math.PI);
+        return [l, x, y, 'right', Math.floor(angle)];
+    }
+    this.init = function() {
+        $(".arrow").each(function(i) {
+            p = get_coordinates(this);
+            classes = this.className.split(' ');
+            direction = way = '';
+            way = p[3];
+            direction = 'horizontal';
+            what = 'width';
+            if($(this).attr('data-x') == undefined) $(this).attr('data-x', p[1])
+            if($(this).attr('data-y') == undefined) $(this).attr('data-y', p[2])
+            if($(this).attr('data-rotate') == undefined) $(this).attr('data-rotate', p[4])
+            css = {}; css[what] = p[0]
+            $(this).addClass('step')
+            $(this).addClass('skip')
+            $(this).css(css)
+            html = ['<div class="arrow-body-common arrow-body-'
+            + direction + '"></div>',
+        '<div class="arrow-end arrow-' +
+            way + '"></div>']
+            $(this).html(html.join(""))
         })
-$("#driver_process").on("impress:stepenter", function(e) {
-            $("#udev").nextAll(".arrow").hide()
-        })
-$("#udev").on("impress:stepenter", function(e) {
-            $("#driver_process").nextAll(".arrow").hide()
-            $("#udev").nextAll(".arrow").show()
-            $("#ls").nextAll(".arrow").hide()
-        })
-$("#ls").on("impress:stepenter", function(e) {
-            $("#driver_process").nextAll(".arrow").hide()
-            $("#ls").nextAll(".arrow").show()
-            $("#driver_process2").nextAll(".arrow").hide()
-        })
-$("#driver_process2").on("impress:stepenter", function(e) {
-            $("#ls").nextAll(".arrow").hide()
-            $("#driver_process2").nextAll(".arrow").show()
-        })
-$("#descriptions").children().each(function() {
-        id = this.className.split("_description")[0];
-        style = document.styleSheets[0]
-        len = style.cssRules.length
-        style.insertRule('.impress-on-' + id + ' .' + this.className + " { opacity: 1}", len);
+        $("#descriptions, #impress").children().each(function() {
+            if(!this.className.match(/_description$/)) return;
+            id = this.className.split("_description")[0];
+            style = document.styleSheets[0]
+            len = style.cssRules.length
+            style.insertRule('.impress-on-' + id + ' .' + this.className + " { opacity: 1}", len);
         style.insertRule('.impress-enabled .' + this.className + " { display: block}", len);
         style.insertRule('.' + this.className + " { opacity: 0}", len);
         $(this).addClass('description')
         })
-
+    }
+    this.load_images = function() {
+var impress_arrow_i = 0;
+$("img").each(
+            function(img)
+            {
+            if(this.src.match(/^arrow:\/\//))
+            {
+            var page = this.src.split("arrow://")[1]
+            var id = 'impress_arrow_' + impress_arrow_i
+            var item = $(this).replaceWith('<iframe src=' + page +
+                ' width=' + this. width + ' height='
+                + this.height + ' frameBorder=0 '+
+                'id=' + id + ' ></iframe>');
+            impress_arrow_i += 1
+            iframe = $('#' + id);
+                iframe.after('<br/>' + 
+'<a href='+ page + '>view ' + this.title + '</a>')
+            }
+            });
+return this;
+    }
+    return this;
+}
